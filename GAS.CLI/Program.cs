98a33e74e30c91bf3569dc4260965c54af39db4a
kasthack <kasthack@epicm.org>
2012-04-28 21:23:33 +0300

    TUI is better now

--- GAS.CLI/Program.cs
+++ GAS.CLI/Program.cs
@@ -7,17 +7,18 @@ namespace GAS.CLI
         static GAS.Core.Manager Core = new GAS.Core.Manager();
        public static void Main(string[] args)
         {
-            while (true)
-            {
                 Console.WriteLine("Select target");
                 string temp = Console.ReadLine();
                 bool IPOK = Core.LockOn(temp);
                 if (!IPOK)
-                    break;
+                {
+                Console.WriteLine("Wrong Target!");
+                Environment.Exit(1);
+                }
                 Console.WriteLine("Subsite is {0}, do you want to change it?[y/n]", Core.Subsite);
                 if (Console.ReadLine().ToLower()[0] == 'y')
                     Core.Subsite = Console.ReadLine();
-    			Console.WriteLine("Select attack type [UDP/TCP/HTTP/ReCoil/SlowLOIC/RefRef]");
+                        Console.WriteLine("Select attack type [UDP/TCP/HTTP/ReCoil/SlowLOIC/RefRef]");
                 temp = Console.ReadLine();
                 if (temp != "RefRef")
                     Core.Method = (GAS.Core.AttackMethod) Enum.Parse(typeof(GAS.Core.AttackMethod), temp);
@@ -32,23 +33,25 @@ namespace GAS.CLI
                 Core.Timeout = int.Parse(Console.ReadLine());
                 Console.WriteLine("Enter thread count[10]");
                 Core.Threads = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter sockets per thread[50]");
+                Console.WriteLine("Enter sockets per thread [50]");
                 Core.SPT = int.Parse(Console.ReadLine());
-                Console.WriteLine("USE Get");
+                Console.WriteLine("USE Get [true]");
                 Core.USEGet = bool.Parse(Console.ReadLine());
-                Console.WriteLine("USE GZIP");
+                Console.WriteLine("USE GZIP [true]");
                 Core.UseGZIP = bool.Parse(Console.ReadLine());
-                Console.WriteLine("Enter delay[0]");
+                Console.WriteLine("Enter delay [0]");
                 Core.Delay = int.Parse(Console.ReadLine());
-                Console.WriteLine("Wait    For Response ");
+                Console.WriteLine("Wait    For Response [false] ");
                 Core.WaitForResponse = bool.Parse(Console.ReadLine());
-                Console.WriteLine("Append RANDOMC hars ");
+                Console.WriteLine("Append RANDOMC hars [true]");
                 Core.AppendRANDOMChars = bool.Parse(Console.ReadLine());
-                Console.WriteLine("Append RANDOM Chars 2 Url ");
+                Console.WriteLine("Append RANDOM Chars 2 Url [true]");
                 Core.AppendRANDOMCharsUrl = bool.Parse(Console.ReadLine());
+                Console.WriteLine("Starting attack");
                 Core.Start();
-				
-            }
+                Console.WriteLine("Attacking...")
+                Console.WriteLine("Press Enter stop attack and exit");
+                Console.ReadLine();
+                Core.Stop();
         }
     }
-}
