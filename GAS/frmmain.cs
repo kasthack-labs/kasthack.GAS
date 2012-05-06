83db84481ce73e9cccef3358b229669aef825b5c
kasthack <kasthack@epicm.org>
2012-05-06 19:37:03 +0300

    usings cleanup

--- GAS/frmmain.cs
+++ GAS/frmmain.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
-using System.Net;
 namespace GAS
 {
     public partial class frmmain : Form
