f3b56c701c37eb917bcb2491eba22cdfa06ef600
kasthack <kasthack@epicm.org>
2012-12-25 03:39:37 +0400

    fast generators with pointers

--- GAS.Core/Strings/Functions.cs
+++ GAS.Core/Strings/Functions.cs
@@ -10,17 +10,14 @@ namespace GAS.Core.Strings
         #region magic!
         //public jfl
         public static char[] _hex_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
-        public static char[] _ascii_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
-                                                'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-                                                'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
-                                                'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                                                '!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';',
-                                                '<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'};
+        public static char[] _ascii_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+                                                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                                                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '!','"','#','$','%','&','\'','(',')','*','+',',','-','.',
+                                                '/',':',';', '<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~'};
         public static byte[] _ascii_chars_bytes = { 48,49,50,51,52,53,54,55,56,57,97,98,99,100,101,102,103,104,105,106,107,
                                                           108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,65,66,67,
                                                           68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,
-                                                          33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,
-                                                          92,93,94,95,96,123,124,125,126};
+                                                          33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,58,59,60,61,62,63,64,91,92,93,94,95,96,123,124,125,126};
         public static byte[] _hex_chars_bytes = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 97, 98, 99, 100, 101, 102, };
         public static ushort[] _offsets = { 0, 0, 33, 48, 65, 97 };
         public static ushort[] _lengs = { 65535, 32, 32, 10, 26, 26 };
@@ -29,10 +26,7 @@ namespace GAS.Core.Strings
         public Functions() {
         }
         public static string RandomString() {
-            char[] ch = new char[6];
-            for ( int i = 0; i < 6; i++ )
-                ch[i] = Convert.ToChar(random.Next(65, 90));
-            return new string(ch);
+            return new string(random_ascii(1, 8, _ascii_chars, 0, 61));
         }
         public static string RandomUserAgent() {
             //slow. very slow
@@ -167,8 +161,9 @@ namespace GAS.Core.Strings
             return __output;
         }
         public static char[] random_ascii(int _len, char[] _source, int _startindex, int _maxindex) {
+            _maxindex++;
             char[] __output = new char[_len];
-            for ( int i = 0; i < _len; __output[i++] = _source[random.Next(_startindex, _maxindex + 1)] ) ;
+            for ( int i = 0; i < _len; __output[i++] = _source[random.Next(_startindex, _maxindex)] ) ;
             return __output;
         }
         /*same but with bytes*/
@@ -257,7 +252,7 @@ namespace GAS.Core.Strings
         public static byte[] random_ascii_bytes(int _len, byte[] _source, int _startindex, int _maxindex) {
             _maxindex++;
             byte[] __output = new byte[_len];
-            for ( int __i = 0; __i < _len; __output[__i++] = _source[random.Next(_startindex, _maxindex + 1)] ) ;
+            for ( int __i = 0; __i < _len; __output[__i++] = _source[random.Next(_startindex, _maxindex)] ) ;
             return __output;
         }
         /*unsafe*/
@@ -299,6 +294,45 @@ namespace GAS.Core.Strings
             };
             return __cnt;
         }
-
+        
+        /*generators with pointers. Warning! NOT TESTED! POTENTIAL CRASH AND LOSS OFF DATA! IT'S NOT CAPSLOCK - IT'S HOLDEN SHIFT!*/
+        public static unsafe void random_utf_urlencode_string_bytes_insert(byte* _ptr, int _real_len) {
+            byte* __end = (_ptr+ _real_len * 6);
+            ushort __rnd = 0;
+            byte __percent = (byte)'%';
+            while ( _ptr < __end) {
+                __rnd = (ushort)random.Next(1, 65535);
+                *_ptr++ = __percent;
+                *_ptr++ = _hex_chars_bytes[__rnd >> 12];
+                *_ptr++ = _hex_chars_bytes[( __rnd >> 8 ) & 0xf];
+                *_ptr++ = __percent;
+                *_ptr++ = _hex_chars_bytes[( __rnd >> 4 ) & 0xf];
+                *_ptr++ = _hex_chars_bytes[__rnd & 0xf];
+            }
+        }
+        public static unsafe void random_ascii_bytes(byte* _ptr, int _len, byte[] _source, int _startindex, int _maxindex) {
+            _maxindex++;
+            byte* __end = ( _ptr + _len );
+            while ( _ptr<__end) *_ptr++ = _source[random.Next(_startindex, _maxindex)];
+        }
+        public static unsafe void random_utf_urlencode_string(char* _ptr, int _real_len) {
+            char* __end = ( _ptr + _real_len * 6 );
+            ushort __rnd = 0;
+            char __pc = '%';
+            while ( _ptr < __end ) {
+                __rnd = (ushort)random.Next(1, 65535);
+                *_ptr++ = __pc;
+                *_ptr++ = _hex_chars[__rnd >> 12];
+                *_ptr++ = _hex_chars[( __rnd >> 8 ) & 0xf];
+                *_ptr++ = __pc;
+                *_ptr++ = _hex_chars[( __rnd >> 4 ) & 0xf];
+                *_ptr++ = _hex_chars[__rnd & 0xf];
+            }
+        }
+        public static unsafe void random_ascii(char* _ptr, int _len, char[] _source, int _startindex, int _maxindex) {
+            _maxindex++;
+            char* __end = ( _ptr + _len );
+            while ( _ptr < __end ) *_ptr++ = _source[random.Next(_startindex, _maxindex)];
+        }
     }
 }
